d2b2945eb0c3f49d62c3a380895160edebe73790
brennochimas <brennochimanski@gmail.com>
2024-10-29 10:20:39 -0300

    Ficha Profissional

---
+++ ficha-Profissional (1)/script.js
@@ -0,0 +1,21 @@
+//VALORES DO HTML ESTÁTICO
+//CGM - EMAIL - IDADE
+const nome=document.getElementById("nome");
+const sobrenome = document.getElementById("sobrenome");
+const cgm = document.getElementById("CGM");
+const idade = document.getElementById("IDADE");
+const eMail = document.getElementById("EMAIL");
+
+//VALORES DE VARIAVEIS 
+const novoNome = "Brenno"
+const Sobrenome2 = "Chimanski"
+const novoCgm = 12345678
+const novaIdade = 16
+const novoeMail = "tetakonacama@hotemail.COM2008"
+
+//NOVOS VALORES ATRIBUIDOS DE FORMA DINÂMICA
+nome.innerHTML=novoNome
+sobrenome.innerHTML = sobrenome2
+cgm.innerHTML = novoCgm
+idade.innerHTML = novaIdade
+email.innerHTML = novoeMail
